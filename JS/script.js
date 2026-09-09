462bdee55ce8c9b017faf95dc2b7eadb2bc79a22
AMALALAWADI212 <amalalawadi666@gmail.com>
2026-09-09 07:34:45 +0300

    describe changes image

--- JS/script.js
+++ JS/script.js
@@ -70,7 +70,7 @@ myImageDiv.classList.add("myImage-div");
 let myImage = document.createElement("img");
 myImage.classList.add("my-image");
 myImageDiv.appendChild(myImage);
-myImage.src = "Images/hero-image.png";
+myImage.src = "Images/hero-Image.jpeg";
 let viewButton  = document.createElement("button");
 viewButton.classList.add("view-button");
 let viewButtonContent = document.createTextNode("View My Work"); 
